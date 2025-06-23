83808c2ded61a5ff2673df2aa4eaa45de64cbb07
CHAVEZ ROBELLY ANNTHONY SEBASTIAN <121054129+AnnthonyChavez@users.noreply.github.com>
2025-06-23 10:46:52 -0500

    Delete consumo_apis directory

--- consumo_apis/contactapi/contactapi/src/main/java/com/example/contactapi/repository/ContactRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.contactapi.repository;
-
-import com.example.contactapi.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {}
